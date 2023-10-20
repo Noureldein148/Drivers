a699914b0d2a3ae61b70258e36ced2912d87d99e
Noureldein148 <111243843+Noureldein148@users.noreply.github.com>
2023-10-20 22:56:45 +0300

    Add files via upload

---
+++ SWitch.c
@@ -0,0 +1,17 @@
+#include "DIO_interface.h"
+#include "BITMATH.h"
+
+void SWitch(void)
+{
+    DIO_SetPinDirection(DIO_PORTA,DIO_PIN6,DIO_PIN_HIGH); // yellow led output
+    DIO_SetPinDirection(DIO_PORTB , DIO_PIN1 , DIO_PIN_LOW);// input pushbutton
+
+    if( GetBit(DIO_PORTB,DIO_PIN1) == 1)
+    {
+        DIO_SetPinValue(DIO_PORTA,DIO_PIN6 , DIO_PIN_HIGH);
+    }
+    else if( GetBit(DIO_PORTB,DIO_PIN1) == 0)
+    {
+        DIO_SetPinValue(DIO_PORTA,DIO_PIN6 , DIO_PIN_LOW);
+    }
+}
---
+++ SevenSegment.h
@@ -0,0 +1,6 @@
+#ifndef SEVENSEGMENT_H_INCLUDED
+#define SEVENSEGMENT_H_INCLUDED
+#include"STD_TYPES_H_.h"
+void SEV_SEG_Init(void);
+void SEV_SEG_Display(u8 data);
+#endif // SEVENSEGMENT_H_INCLUDED
