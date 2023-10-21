09059acb1cb491c67cf7d3cd08d281f0236bd6e5
Noureldein148 <111243843+Noureldein148@users.noreply.github.com>
2023-10-21 06:44:55 +0300

    Update Seven_Segment_private.h

--- Seven_Segment_private.h
+++ Seven_Segment_private.h
@@ -1,3 +1,10 @@
+/************************************************************
+*   Author  : Nour El Dein Hany
+*   Layer   : HAL
+*   SWC     : Seven Segment
+*   Date    : 20/10/2023
+*   Version : 1.0
+*/
 #ifndef SEVEN_SEGMENT_PRIVATE_H_INCLUDED
 #define SEVEN_SEGMENT_PRIVATE_H_INCLUDED
 
