979b890e91ff61f430077a329388a5475d44a4fd
Noureldein148 <111243843+Noureldein148@users.noreply.github.com>
2023-10-21 05:36:41 +0300

    Delete SevenSegment.h

--- SevenSegment.h
+++
@@ -1,6 +0,0 @@
-#ifndef SEVENSEGMENT_H_INCLUDED
-#define SEVENSEGMENT_H_INCLUDED
-#include"STD_TYPES_H_.h"
-void SEV_SEG_Init(u8 com);
-void SEV_SEG_Display(u8 data);
-#endif // SEVENSEGMENT_H_INCLUDED
