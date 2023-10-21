6b6b608d57c49e196b4f0e53350880c1567d24e1
Noureldein148 <111243843+Noureldein148@users.noreply.github.com>
2023-10-21 06:44:42 +0300

    Update SevenSegment_Config.h

--- SevenSegment_Config.h
+++ SevenSegment_Config.h
@@ -1,3 +1,10 @@
+/************************************************************
+*   Author  : Nour El Dein Hany
+*   Layer   : HAL
+*   SWC     : Seven Segment
+*   Date    : 20/10/2023
+*   Version : 1.0
+*/
 #ifndef SEVENSEGMENT_CONFIG_H_INCLUDED
 #define SEVENSEGMENT_CONFIG_H_INCLUDED
 #include"DIO_interface.h"
